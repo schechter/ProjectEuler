1731639b7a6a7a0151549c58c67330480f3f009e
Mike Schechter <schechtermike@gmail.com>
2013-08-15 13:44:57 -0400

    4-5 more problems

--- project_euler_helpers.rb
+++ project_euler_helpers.rb
@@ -74,63 +74,112 @@ def triangle_num_gen(n)
   (n * (n + 1 ))/2
 end
 
-# def divisor_counter(n)
-#   require 'prime'
-#   divisors = ['self']
-#   unless Prime.instance.prime?(n)
-#     for i in (1..n/2)
-  #       if n % i == 0
-  #         divisors << i
-  #       end
-  #     end
-  #   end
-  #   p "#{n} has #{divisors.count} divisors"
-  #   divisors.count
-  # end
 
-  def divisor_counter(n)
-    divisors = 2
-    require 'prime'
-    for i in (2..(n**0.5))
-      if n % i == 0
-        divisors += 2
-      end
+def divisor_counter(n)
+  divisors = 2
+  for i in (2..(n**0.5))
+    if n % i == 0
+      divisors += 2
     end
-    divisors
   end
+  divisors
+end
 
-  def collatz_sequence(n)
-    sequence = [n]
-    while n != 1
-      if n.even?
-        n = n/2
-        sequence << n
-      else
-        n = ((3 * n) + 1)
-        sequence << n
-      end
-    end
-    sequence.count
+def collatz_sequence(n)
+  sequence = [n]
+  while n != 1
+    if n.even?
+      n = n/2
+  sequence << n
+  else
+    n = ((3 * n) + 1)
+    sequence << n
   end
+end
+sequence.count
+end
 
-  def prob18recursion(triangle, current_node, paths, path_length) #Find the maximum total from top to bottom of the triangle below:
-    path_length = path_length || 0
-    path_length += triangle[current_node[0]][current_node[1]]
-    if current_node[0] == triangle.length- 1
-      return path_length
-    else
+def prob18recursion(triangle, current_node, paths, path_length) #Find the maximum total from top to bottom of the triangle below:
+  path_length = path_length || 0
+  path_length += triangle[current_node[0]][current_node[1]]
+  if current_node[0] == triangle.length- 1
+    return path_length
+  else
     # unless current_node[0] == triangle.length-1
-      paths << prob18recursion(triangle, [current_node[0]+1, current_node[1]+1], paths, path_length)
-      paths << prob18recursion(triangle, [current_node[0]+1, current_node[1]], paths, path_length)
+    paths << prob18recursion(triangle, [current_node[0]+1, current_node[1]+1], paths, path_length)
+    paths << prob18recursion(triangle, [current_node[0]+1, current_node[1]], paths, path_length)
+  end
+end
+
+def is_sunday(n)
+  if n % 7 == 0
+    1
+  else
+    0
+  end
+end
+
+def is_amicable(n)
+  x = sum_of_divisors(n)
+  if sum_of_divisors(x) == n && x != n
+    return x, n
+  end
+  0
+end
+
+def sum_of_divisors(n)
+  sum = 0
+  for i in (1..((n/2) +1))
+    if n % i == 0
+      sum += i
     end
   end
+  sum
+end
 
-  def is_sunday(n)
-    if n % 7 == 0
-      1
-    else
-      0
+def is_abundant_number?(n)
+  if n < sum_of_divisors(n)
+    return true
+  end
+  false
+end
+
+def is_sum_of_two_from_array(n, array)
+  for i in array
+    j = n - i
+    if array.include? j
+      return true
     end
   end
+  false
+end
 
-  
+def get_subsets(array)
+  if array.length == 0
+    return [[]]
+  end
+  subsets = []
+  first = array.first
+  rest = array.drop(1)
+  get_subsets(rest).each do |partial_subset|
+    subsets << partial_subset
+    next_subset = partial_subset + [first]
+    subsets << next_subset
+  end
+  subsets
+end
+
+def is_pandigital(a,b,c)
+  a, b, c = a.to_s.chars, b.to_s.chars, c.to_s.chars
+  c << a << b
+  c.flatten!.sort!
+  if c.length == 9 && c[0] =='1' && c[1] =='2' && c[2] =='3' && c[3] == '4' && c[4] =='5' && c[5] == '6' && c[6] =='7' && c[7] =='8' && c[8] =='9'
+    return true
+  end
+  false
+end
+
+def factorial(num)
+  (2..num).inject(1) { |f, n| f * n }
+end
+
